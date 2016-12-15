c996c3f67d717a65ae7e73a5a9c3b477ccd4a564
BohdanMoroz <bohdanmoroz@gmail.com>
2016-12-16 00:32:09 +0200

    Лаб 1

---
+++ Lab_1/src/com/company/Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JButton btn = new JButton("Start");
+        JFrame frame = new JFrame("First");
+        JTextField tfield1 = new JTextField("Here is your text");
+        JTextField tfield2 = new JTextField("");
+        JPanel panel = new JPanel();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400,50);
+        frame.setVisible(true);
+        frame.add(panel, BorderLayout.NORTH);
+
+        panel.setLayout(new BorderLayout());
+        panel.add(tfield1, BorderLayout.CENTER);
+        panel.add(tfield2, BorderLayout.SOUTH);
+        panel.add(btn, BorderLayout.EAST);
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = tfield1.getText();
+                tfield2.setText(text.substring(0,text.length()/2).toUpperCase() + text.substring(text.length() / 2,text.length()));
+            }
+        });
+    }
+}
